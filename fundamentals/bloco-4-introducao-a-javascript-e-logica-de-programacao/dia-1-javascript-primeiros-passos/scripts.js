bc4dcfeebaedefbfb239eaf98038e6a7b3dd3e46
Elimar Lucena <elimarlucenar@gmail.com>
2021-11-07 19:46:10 -0300

    exercises of JavaScript

---
+++ fundamentals/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,111 @@
+/*
+const a = 9;
+const b = 3;
+const c = 5;
+*/
+
+/*
+console.log(`The sum between a and b is = ${a + b};`);
+console.log(`The subtraction between a and b is = ${a - b};`);
+console.log(`The multiplication between a and b is = ${a * b};`);
+console.log(`The division between a and b is = ${a / b};`);
+console.log(`The module between a and b is = ${a % b};`);
+*/
+
+/*
+if (a > b) {
+    console.log(`${a} 0{"}0`)
+}else {
+    console.log(`${b} 0{"}0`)
+}
+*/
+
+/*
+if (a > b && a > c) {
+    console.log(`${a} 0{"}0`)
+}if (b > a && b > c) {
+    console.log(`${b} 0{"}0`)
+}if (c > a && c > b) {
+    console.log(`${c} 0{"}0`)
+}
+*/
+/*
+const value = -23;
+if (value >= 1) {
+    console.log(`Positive`);
+}else if (value < 0) {
+    console.log(`Negative`);
+}else {
+    console.log(`0`);
+}
+*/
+
+/*
+const angleOne = 50;
+const angleTwo = 50;
+const angleThree = 60;
+
+if ((angleOne + angleTwo + angleThree) === 180) {
+    console.log(`True`)
+}else {
+    console.log(`False`)
+}
+*/
+
+/*
+let a = "King";
+let chess = a.toLowerCase();
+
+switch (chess) {
+    case "king":
+        console.log("move to all searches vertically, horizontally or diagonally, but only one square per move.")
+        break;
+    case "queen":
+        console.log("Like the King, it can move in any direction (vertical, horizontal and diagonal), however as many squares as you like, as long as they are free.");
+        break;
+    case "bishop":
+        console.log("It moves diagonally, not being able to jump intervening pieces, and captures taking the place occupied by the opposing piece.");
+        break;
+    case "knigth":
+        console.log(`The move performed by the Knight is known as "one-two" or "in L". ... He can walk two squares horizontally and one vertically, or two vertically and one horizontally, one horizontally and two vertically`);
+        break;
+    case "rook":
+        console.log("The rook moves back and forth, right and left, any squares it likes, but it cannot jump any other pieces.");
+        break;
+    case "pawn":
+        console.log("Pawns can only move one square at a time, except on their first moves, when they can move two squares. Pawns can only capture on the diagonal squares in front of them. They can never capture back. If there is a piece immediately in front of the pawn, it cannot move or capture that piece.");
+        break;
+    default:
+        console.log("ERRO")
+        break;
+}
+*/
+
+/*
+let note = 50;
+
+if (note >= 90) {
+    console.log("A");
+}else if (note >= 80) {
+    console.log("B");
+}else if (note >= 70) {
+    console.log("C");
+}else if (note >= 60) {
+    console.log("D");
+}else if (note >= 50) {
+    console.log("E");
+}else if (note >= 0 && note < 50){
+    console.log("F");
+}else {
+    console.log("ERRO");
+}
+*/
+
+const numberOne = 4;
+const numberTwo = 8;
+const numberThree = 9;
+numberOne % 2;
+
+if (numberOne === 2) {
+    console.log("true");
+}
