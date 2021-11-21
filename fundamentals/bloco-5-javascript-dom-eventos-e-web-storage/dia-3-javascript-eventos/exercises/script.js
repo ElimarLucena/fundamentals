c06943c7d0d3b5c3aa09dbb2d9a562369f89ba80
Elimar Lucena <elimarlucenar@gmail.com>
2021-11-21 14:42:11 -0300

    exercise 1

---
+++ fundamentals/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/script.js
@@ -0,0 +1,55 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+
+for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+};
+};
+
+createDaysOfTheWeek();
+
+  //exercise 1:
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    
+function dayCalender (dayList){
+    let ulList = document.getElementById('days');
+    let days;
+    let onLi
+    for (let index = 0; index < dayList.length; index += 1){
+        days = dayList[index];
+        onLi = document.createElement('li');
+        onLi.classList.add('day');
+        onLi.innerText = days;
+        ulList.appendChild(onLi);
+
+        switch (days){
+            case 24:
+                onLi.classList.add('holiday');
+                break;
+            case 25:
+                onLi.classList.add('holiday');
+                onLi.classList.add('friday');
+                break;
+            case 31:
+                onLi.classList.add('holiday');
+                break;
+            case 4:
+                onLi.classList.add('friday');
+                break;
+            case 11:
+                onLi.classList.add('friday');
+                break;
+            case 18:
+                onLi.classList.add('friday');
+                break;
+        }
+    }
+}
+dayCalender(dezDaysList);
+
+//exercise 2:
